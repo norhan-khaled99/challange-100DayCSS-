b5bf22c2b66e77403cee7421524fb6732da16ac0
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-03-19 00:45:13 +0200

    new challange ✌️
    simple gradiant background with some animation to buttons

---
+++ script.js
@@ -0,0 +1,18 @@
+/*var x=window.prompt();
+var y=window.prompt();
+var z=Number(x)+Number(y);
+alert(z);
+
+function foo()
+{
+
+var bar =function bar() {return 3;}
+return bar();
+var bar=function bar() {return 8;}
+}
+alert(foo())
+*/
+const one=false||{}||null;
+const two =null||false ||"";
+const three=[] || 0 ||true
+console.log(one , two , three);
